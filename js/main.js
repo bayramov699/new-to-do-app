1f0bcda05fb8b0750c04e492067dda741ded2abd
bayramov699 <bayramov699@gmail.com>
2020-08-31 19:04:48 +0400

    last change

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// Jquery Functions
 $(document).ready(() => {
   const input = $("#input");
   const submit = $("#submit");
@@ -11,14 +12,17 @@ $(document).ready(() => {
     for (let task in newObj["datas"]) {
       let todo = `
           <li>
-          <span id="todo-item">${newObj["datas"][task]}</span>
-          <button type="button" onclick="ready(this)" class="btn btn-success">DONE</button><br>
+          <span id="todo-item">${newObj["datas"][task]}</span>         
+          <button id="todoEdit" type="button" onclick="makeEdit(this)" class="btn btn-info">EDIT</button>
+          <button type="button" onclick="ready(this)" class="btn btn-success" style="margin-left:10px;">DONE</button>
           </li>`;
       $(todo).appendTo(listItems);
     }
   }
-
+  
+// ADD button
   submit.on("click", () => {
+    let storedObj = localStorage.getItem("todos");
     let todoObj = {
       datas: [],
     };
@@ -32,13 +36,34 @@ $(document).ready(() => {
     }
     let todo = `<li>
       <span>${input.val()}</span>
-      <button type="button" onclick="ready(this)" class="btn btn-success">DONE</button><br>
+      <button id="todoEdit" type="button" onclick="makeEdit(this)" class="btn btn-info">EDIT</button>
+      <button type="button" onclick="ready(this)" class="btn btn-success" style="margin-left:10px;">DONE</button>
       </li>`;
     $(todo).appendTo(listItems);
     input.val("");
   });
 });
+// Helper Function-1
+function makeEdit(e) {
 
+  $(e).parent().hide();
+  
+  let localArr = [];
+  let storedObj = JSON.parse(localStorage.getItem('todos'));
+  const data = $(e).parent().find('span').text();
+
+  for (let task in storedObj['datas']) {
+      if (data !== storedObj['datas'][task]) {
+          localArr.push(storedObj['datas'][task]);
+      }
+  }
+  storedObj['datas'] = localArr;
+
+  localStorage.setItem('todos', JSON.stringify(storedObj));
+
+  $('#input').val(data);
+};
+// Helper Function-2
 function ready(e) {
   let localArr = [];
   let storedObj = JSON.parse(localStorage.getItem("todos"));
@@ -56,6 +81,7 @@ function ready(e) {
   $(e).parent().find("span").css("text-decoration", "line-through");
 }
 
+// Plugin library
 const slideshow = new Slideshow({
   tickInterval: 5000,
   transitionTime: 100,
